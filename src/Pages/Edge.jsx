814c83cb151e1db3739f098829292e71cf4f155c
HarshalTarwale <tarwaleh@gmail.com>
2026-02-09 22:28:09 +0530

    edge page link added of home grid

--- src/Pages/Edge.jsx
+++ src/Pages/Edge.jsx
@@ -3,6 +3,10 @@ import React from 'react'
 const Edge = () => {
   return (
     <div className='w-full min-h-[100vh]'>
+        {/* Navbar */}
+        <Navbar/>
+
+        
       
     </div>
   )

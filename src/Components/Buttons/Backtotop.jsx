7e8dccf4bd6855d44278750562cd9adecbde2f89
HarshalTarwale <tarwaleh@gmail.com>
2026-02-12 01:21:09 +0530

    back to top button complected

--- src/Components/Buttons/Backtotop.jsx
+++ src/Components/Buttons/Backtotop.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { FaArrowUp } from "react-icons/fa";
 
 const Backtotop = () => {
   const [isVisible, setIsVisible] = useState(false);
@@ -31,10 +32,10 @@ const Backtotop = () => {
       {isVisible && (
         <button
           onClick={scrollToTop}
-          className='fixed flex items-center gap-[2vh] bottom-8 right-8 z-50 bg-white/10 backdrop-blur-md border border-white/30 text-white px-1 pl-4 py-1 rounded-full hover:bg-white/20 transition-all duration-300 zen text-sm shadow-lg hover:scale-105'
+          className='fixed flex items-center gap-[2vh] bottom-10 left-1/2 -translate-x-1/2 z-50 bg-white/10 backdrop-blur-md border border-white/30 text-white px-1 pl-4 py-1 rounded-full hover:bg-white/20 transition-all duration-300 zen text-sm shadow-lg hover:scale-105'
           aria-label="Back to top"
         >
-        Back To Top <div className='h-[7vh] w-[7vh] rounded-[1000px] bg-gray-200'></div>
+        Back To Top <div className='h-[7vh] w-[7vh] rounded-[1000px] bg-gray-200 flex items-center justify-center text-black'><FaArrowUp className='text-[3vh]'/></div>
         </button>
       )}
     </>

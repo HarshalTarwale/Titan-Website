e6a5c7e9970bfc64f77a24dfbf47e312ca6233a6
HarshalTarwale <tarwaleh@gmail.com>
2026-02-13 01:09:52 +0530

    back to top button updated

--- src/Components/Buttons/Backtotop.jsx
+++ src/Components/Buttons/Backtotop.jsx
@@ -35,7 +35,7 @@ const Backtotop = () => {
           className='fixed flex items-center gap-[2vh] bottom-10 left-1/2 -translate-x-1/2 z-50 bg-white/10 backdrop-blur-md border border-white/30 text-white px-1 pl-4 py-1 rounded-full hover:bg-white/20 transition-all duration-300 zen text-sm shadow-lg hover:scale-105'
           aria-label="Back to top"
         >
-        Back To Top <div className='h-[7vh] w-[7vh] rounded-[1000px] bg-gray-200 flex items-center justify-center text-black'><FaArrowUp className='text-[3vh]'/></div>
+        Back To Top <div className='h-[7vh] w-[7vh] rounded-[1000px] bg-white flex items-center justify-center text-black'><FaArrowUp className='text-[3vh]'/></div>
         </button>
       )}
     </>
